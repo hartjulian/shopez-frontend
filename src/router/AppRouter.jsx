75bcf1bbce8e75a4041b20e4d2e89a32eaaac915
Julian Hart <julian.w.hart@gmail.com>
2026-03-31 16:27:02 +1300

    ScrollToTop component ensures View All Products navigates to top of page.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,10 +4,12 @@ import Cart from "../pages/Cart";
 import Home from "../pages/Home";
 import Products from "../pages/Products";
 import ProductDetails from "../pages/ProductDetails";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <Routes>
         <Route element={<Layout />} >
           <Route path="/" element={<Home />} />
